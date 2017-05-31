84d98d3f1326da6244f27328b03af6c2ea87f485
Austin <austin346@gmail.com>
2017-05-31 13:11:49 -0500

    Add Rust RakNet API

--- hapis/main.cpp
+++ hapis/main.cpp
@@ -1,6 +1,21 @@
 #include <Windows.h>
+#include <iostream>
+#include <inttypes.h>
+
+#include "rrapi.h"
 
 int main()
 {
+	try
+	{
+		RustNetAPI::Init();
 
+		// TODO: Proxy & hacks
+		return 0;
+	}
+	catch (std::exception e)
+	{
+		printf("ERROR: %s\n", e.what());
+		return 1;
+	}
 }
