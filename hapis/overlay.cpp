cf40b15b73771c84a2a26ec57b437d32393c1153
nathan <argoeclipse@gmail.com>
2017-06-06 23:34:37 -0400

    fix winproc

--- hapis/overlay.cpp
+++ hapis/overlay.cpp
@@ -128,49 +128,37 @@ namespace Overlay {
 	/* Fuck WinAPI */
 
 	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-		ImGuiIO& io = ImGui::GetIO();
 		switch (msg)
 		{
-		case WM_LBUTTONDOWN:
-			io.MouseDown[0] = true;
-			return true;
-		case WM_LBUTTONUP:
-			io.MouseDown[0] = false;
-			return true;
-		case WM_RBUTTONDOWN:
-			io.MouseDown[1] = true;
-			return true;
-		case WM_RBUTTONUP:
-			io.MouseDown[1] = false;
-			return true;
-		case WM_MBUTTONDOWN:
-			io.MouseDown[2] = true;
-			return true;
-		case WM_MBUTTONUP:
-			io.MouseDown[2] = false;
-			return true;
-		case WM_MOUSEWHEEL:
-			io.MouseWheel += GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? +1.0f : -1.0f;
-			return true;
-		case WM_MOUSEMOVE:
-			io.MousePos.x = (signed short)(lParam);
-			io.MousePos.y = (signed short)(lParam >> 16);
-			return true;
-		case WM_KEYDOWN:
-			if (wParam < 256)
-				io.KeysDown[wParam] = 1;
-			return true;
-		case WM_KEYUP:
-			if (wParam < 256)
-				io.KeysDown[wParam] = 0;
-			return true;
-		case WM_CHAR:
-			// You can also use ToAscii()+GetKeyboardState() to retrieve characters.
-			if (wParam > 0 && wParam < 0x10000)
-				io.AddInputCharacter((unsigned short)wParam);
-			return true;
+		case WM_PAINT:
+			render();
+			break;
+
+		case WM_CREATE:
+			DwmExtendFrameIntoClientArea(hWnd, &margin);
+			break;
+
+		case WM_DESTROY:
+			PostQuitMessage(1);
+			return 0;
+		case WM_SIZE:
+			if (p_Device != NULL && wParam != SIZE_MINIMIZED)
+			{
+				ImGui_ImplDX9_InvalidateDeviceObjects();
+				p_Params.BackBufferWidth = LOWORD(lParam);
+				p_Params.BackBufferHeight = HIWORD(lParam);
+				HRESULT hr = p_Device->Reset(&p_Params);
+				if (hr == D3DERR_INVALIDCALL)
+					IM_ASSERT(0);
+				ImGui_ImplDX9_CreateDeviceObjects();
+			}
+			return 0;
+		case WM_SYSCOMMAND:
+			if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+				return 0;
+			break;
 		default:
-			return DefWindowProc(hwnd, msg, wParam, lParam);
+			return DefWindowProc(hWnd, msg, wParam, lParam);
 			break;
 		}
 		return 0;
